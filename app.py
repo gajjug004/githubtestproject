be4cfe03f77f2723b554ac46050e8c7366062d01
Gajendra Nishad <gajjug004@gmail.com>
2024-02-22 11:16:08 +0530

    test story

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, World"
 
+@app.route('/welcome')
+def welcome():
+    return "Welcome to My World"
+
 if __name__ == '__main__':
     app.run(debug=True)
